63e2e7613110666fa9ba3aa21692598612c9cb47
Danilo Pedraza <cpedraza@unal.edu.co>
2024-02-17 17:27:51 -0500

    remove code from main.go

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"lang/own"
-	"lang/parsing"
-	"os"
-
-	"github.com/antlr4-go/antlr/v4"
-)
+import "fmt"
 
 func main() {
-	input, _ := antlr.NewFileStream(os.Args[1])
-	lexer := parsing.NewlangLexer(input)
-	stream := antlr.NewCommonTokenStream(lexer, 0)
-	p := parsing.NewlangParser(stream)
-	tree := p.Program()
-	visitor := &own.Visitor{}
-	visitor.Visit(tree)
-	antlr.ParseTreeWalkerDefault.Walk(&own.Listener{}, tree)
+	fmt.Println("This is work in progress...")
 }
